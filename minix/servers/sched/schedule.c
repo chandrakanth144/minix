a7f89604dd938eb25182f513688a36832a689201
chandrakanth144 <180010003@iitdh.ac.in>
2021-02-03 12:42:55 +0530

    swap added

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -304,6 +304,10 @@ static int schedule_process(struct schedproc * rmp, unsigned flags)
 
 	pick_cpu(rmp);
 
+	if(rmp->priority >= 7){
+		printf("Minix: PID %d swapped in\n",_ENDPOINT_P(rmp->endpoint));
+	}
+
 	if (flags & SCHEDULE_CHANGE_PRIO)
 		new_prio = rmp->priority;
 	else
